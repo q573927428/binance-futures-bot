94bbec78bbac290047e49ca00d5f6c05c5d86509
q573927428 <573927428@qq.com>
2026-03-31 12:41:55 +0800

    ``` feat(kline): 移除时间框架标签并更新K线数据
    移除K线图表组件中的时间框架标签文本和相关样式，
    同时更新所有加密货币的数据集以反映最新的K线信息。
    ```

--- app/components/KLineChartSimple.vue
+++ app/components/KLineChartSimple.vue
@@ -6,7 +6,6 @@
         <div class="symbol-badge">{{ displaySymbol }}</div>
 
         <div class="timeframe-selector">
-          <span class="timeframe-label">周期:</span>
           <div class="timeframe-buttons">
             <el-button
               size="small"
@@ -1034,11 +1033,6 @@ onUnmounted(() => {
   gap: 8px;
 }
 
-.timeframe-label {
-  font-size: 14px;
-  color: #606266;
-  font-weight: 500;
-}
 
 .timeframe-buttons {
   display: flex;
@@ -1277,12 +1271,6 @@ onUnmounted(() => {
   
   .timeframe-selector {
     width: 100%;
-  }
-  
-  .timeframe-label {
-    display: block;
-    margin-bottom: 4px;
-    font-size: 12px;
   }
   
   .timeframe-buttons {
